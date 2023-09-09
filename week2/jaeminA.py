c8155049fb8514488ec1cc771984da9d97fa77f3
1120jm <128223482+1120jm@users.noreply.github.com>
2023-09-09 15:47:59 +0900

    Create jaeminA.py

---
+++ week2/jaeminA.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = [0] * n
+sum = 0
+top = -1
+
+for i in range(n):
+    m = int(input())
+    if m == 0:
+        if top >= 0:
+            arr[top] = 0
+            top -= 1
+    else:
+        top += 1
+        arr[top] = m
+
+for i in range(top+1):
+    sum += arr[i]
+
+print(sum)
+        
+        
